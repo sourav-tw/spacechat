47adb311c96e4f74722326ae093c02a89c88c9e8
Nikhil Samant <nikhilsamant4@gmail.com>
2024-04-05 13:34:25 +0530

    Refactor code

--- config.py
+++ config.py
@@ -7,4 +7,16 @@ INIT_INDEX = os.getenv('INIT_INDEX', 'false').lower() == 'true'
 INDEX_PERSIST_DIRECTORY = os.getenv('INDEX_PERSIST_DIRECTORY', "./data/chromadb")
 
 # http api port
-HTTP_PORT = os.getenv('HTTP_PORT', 7654)
+HTTP_PORT = os.getenv('HTTP_PORT', 5000)
+
+LLM_MODEL = os.getenv('LLM_MODEL', "llama2")
+
+EMBED_MODEL = os.getenv('EMBED_MODEL', "BAAI/bge-small-en-v1.5")
+
+CHUNK_SIZE = int(os.getenv('CHUNK_SIZE', 1000))
+
+CHUNK_OVERLAP = int(os.getenv('CHUNK_OVERLAP', 150))
+
+REQUEST_TIMEOUT = int(os.getenv('REQUEST_TIMEOUT', 60))
+
+DOCUMENTS_DIR = os.getenv('DOCUMENTS_DIR', "./docs")
